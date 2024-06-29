a1e823afb0fd442b08ba5d49fa036fec48d22acd
Priyanshi-Deora <122980126+Priyanshi-Deora@users.noreply.github.com>
2024-06-29 20:31:07 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+const words = [
+    "cottagecore",
+    "fairycore",
+    "baddie",
+    "dark academia",
+    "grunge",
+    "indie aesthetic",
+    "light academia",
+    "soft girl",
+    "vsco aesthetic",
+    "coquette",
+    "preppy",
+    "minimalist",
+    "rustic",
+    "vintage",
+    "contemporary"
+];
+
+let randomIndex = Math.floor(Math.random() * words.length); 
+let selectedWord = words[randomIndex]; 
+console.log(selectedWord); 
+
+// To store the already guessed letters 
+let guessedlist = []; 
+
+let displayWord = ""; 
+for (let i = 0; i < selectedWord.length; i++) { 
+	displayWord += "_ "; 
+} 
+document.getElementById("displayWord").textContent = displayWord; 
+
+function guessLetter() { 
+	let inputElement = document.getElementById("letter-input"); 
+
+	// To check empty input 
+	if (!inputElement.value) { 
+		alert("Empty Input box. Please add input letter"); 
+		return; 
+	} 
+
+	let letter = inputElement.value.toLowerCase(); 
+
+	
+	inputElement.value = ""; 
+
+
+	if (guessedlist.includes(letter)) { 
+		alert("You have already guessed that letter!"); 
+		return; 
+	} 
+
+	
+	guessedlist.push(letter); 
+
+	let updatedDisplay = ""; 
+	let allLettersGuessed = true; 
+	for (let i = 0; i < selectedWord.length; i++) { 
+		if (guessedlist.includes(selectedWord[i])) { 
+			updatedDisplay += selectedWord[i] + " "; 
+		} else { 
+			updatedDisplay += "_ "; 
+			allLettersGuessed = false; 
+		} 
+	} 
+	document.getElementById("displayWord") 
+		.textContent = updatedDisplay; 
+
+	
+	if (allLettersGuessed) { 
+		alert("Congratulations! You guessed the word correctly!"); 
+	} 
+} 
